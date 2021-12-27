1a68e4b5f28d6fd39bf352b40e2bdf2d222ff212
yunda1970 <yundalestaripuji@gmail.com>
2021-12-27 12:19:10 +0700

    coba mengahapus lagi

--- Pesawat3/main.cpp
+++ Pesawat3/main.cpp
@@ -1,20 +1,4 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- * komen 1 tes
- * komen 2 yaa bisa pliss
- * yaa mudah"an nda eror
- * yaaaaaa hmhmhm
- */
+
 #include <windows.h>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
